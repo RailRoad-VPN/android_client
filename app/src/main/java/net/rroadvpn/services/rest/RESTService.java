6f6fe35759b75085c73c7543ea1146f57d3ec787
Artur Karapetov <858683@gmail.com>
2018-08-29 20:55:48 +0300

    //WIP types for maps

--- app/src/main/java/net/rroadvpn/services/rest/RESTService.java
+++ app/src/main/java/net/rroadvpn/services/rest/RESTService.java
@@ -3,43 +3,80 @@ package net.rroadvpn.services.rest;
 import net.rroadvpn.model.rest.RESTResponse;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map;
 
 public class RESTService implements RESTServiceI {
 
-    private HashMap headers;
+    private Map<String, String> headers = new HashMap<>();
     private String serviceURL;
 
+    public RESTService(String serviceURL) {
+        this.serviceURL = serviceURL;
+    }
 
     @Override
-    public RESTResponse get(String url, HashMap data, HashMap headers) {
-        this.prepare(url, headers);
+    public RESTResponse get(String url, Map<String, Object> data, Map<String, String> headers) {
+        if (url == null) {
+            url = this.serviceURL;
+        }
+        if (headers != null) {
+            this.headers.putAll(headers);
+        }
         RESTResponse response = new RESTResponse();
         return response;
     }
 
     @Override
-    public RESTResponse put(String url, HashMap data, HashMap headers) {
-        this.prepare(url, headers);
+    public RESTResponse put(String url, Map<String, Object> data, Map<String, String> headers) {
+        if (url == null) {
+            url = this.serviceURL;
+        }
+        if (headers != null) {
+            this.headers.putAll(headers);
+        }
         RESTResponse response = new RESTResponse();
         return response;
     }
 
     @Override
-    public RESTResponse post(String url, HashMap data, HashMap headers) {
-        this.prepare(url, headers);
+    public RESTResponse post(String url, Map<String, Object> data, Map<String, String> headers) {
+        if (url == null) {
+            url = this.serviceURL;
+        }
+        if (headers != null) {
+            this.headers.putAll(headers);
+        }
         RESTResponse response = new RESTResponse();
         return response;
     }
 
     @Override
-    public RESTResponse delete(String url, HashMap data, HashMap headers) {
-        this.prepare(url, headers);
+    public RESTResponse delete(String url, Map<String, Object> data, Map<String, String> headers) {
+        if (url == null) {
+            url = this.serviceURL;
+        }
+        if (headers != null) {
+            this.headers.putAll(headers);
+        }
         RESTResponse response = new RESTResponse();
         return response;
     }
 
-    private void prepare(String url, HashMap headers) {
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
 
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public String getServiceURL() {
+        return serviceURL;
+    }
+
+    public void setServiceURL(String serviceURL) {
+        this.serviceURL = serviceURL;
     }
 }
 
--- app/src/main/java/net/rroadvpn/services/rest/RESTServiceI.java
+++ app/src/main/java/net/rroadvpn/services/rest/RESTServiceI.java
@@ -3,10 +3,11 @@ package net.rroadvpn.services.rest;
 import net.rroadvpn.model.rest.RESTResponse;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public interface RESTServiceI {
-    RESTResponse get(String url, HashMap data, HashMap headers);
-    RESTResponse put(String url, HashMap data, HashMap headers);
-    RESTResponse post(String url, HashMap data, HashMap headers);
-    RESTResponse delete(String url, HashMap data, HashMap headers);
+    RESTResponse get(String url, Map<String, Object> data, Map<String, String> headers);
+    RESTResponse put(String url, Map<String, Object> data, Map<String, String> headers);
+    RESTResponse post(String url, Map<String, Object> data, Map<String, String> headers);
+    RESTResponse delete(String url, Map<String, Object> data, Map<String, String> headers);
 }
